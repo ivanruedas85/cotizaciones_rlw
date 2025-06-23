c9f02a6aa318e5d69829f635ce69340734347c3f
ivanruedas85 <ivanruedas85@gmail.com>
2025-06-23 13:58:42 -0600

    Se creo la constante de fecha incial y se agrego al copy

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,10 +1,11 @@
-import {SITE_LARGE_TITLE} from "@/utils/const.ts"
+import { SITE_LARGE_TITLE, INITIAL_DATE } from "@/utils/const.ts"
+
 export function Footer() {
     return (
         <footer className="border-t bg-background">
             <div className="max-w-6x1 mx-auto px-4 py-6">
                 <div className="text-center text-sm text-muted-foreground">
-                    &copy; {new Date().getFullYear().toPrecision()} {SITE_LARGE_TITLE}. Todos los derechos reservados.
+                    &copy; {INITIAL_DATE}-{new Date().getFullYear().toPrecision()} {SITE_LARGE_TITLE}. Todos los derechos reservados.
                 </div>
             </div>
         </footer>

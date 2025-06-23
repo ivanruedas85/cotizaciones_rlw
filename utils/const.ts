422d640f6e696f6357b39e4df81eb39c3ebd2a7f
ivanruedas85 <ivanruedas85@gmail.com>
2025-06-23 14:00:47 -0600

    la cambio la fecha en la const INITIAL_DATE

--- utils/const.ts
+++ utils/const.ts
@@ -1,4 +1,4 @@
 export const SITE_LARGE_TITLE = "Rueda Leather Wallet's"
 export const SITE_SMALL_TITLE = "RLW"
 export const DESCRIPTION_SITE = `Sistema de cotizaciones y ventas de ${SITE_LARGE_TITLE}`
-export const INITIAL_DATE = 2022
+export const INITIAL_DATE = 2021
